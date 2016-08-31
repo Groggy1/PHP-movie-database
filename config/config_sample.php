e5fd2fa132b61ef3a2b8f5713e02fbd77d84d50b
Groggy <groggy12@gmail.com>
2016-08-31 09:46:09 +0200

    Added sample config file

---
+++ config/config_sample.php
@@ -0,0 +1,15 @@
+<?php
+define('URL', '/filmdb/');
+define('DB_FLAG', '');
+define('SITE', 'MOVIEDB');
+define('TITLE', 'FilmDB | ');
+define("starting_time_measure",MICROTIME(TRUE));
+
+//DB-configuration
+define('SQL_ENGINE', 'mysql');
+define('SQL_HOST', 'localhost');
+define('SQL_DB', 'filmdb');
+define('SQL_USER', 'root');
+define('SQL_PASS', 'abc123');
+define('SQL_PORT', '3306');
+define('SQL_ENGINE', 'mysql');
